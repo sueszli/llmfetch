d90cd2bb1c7e47d3fa4f9a15c0332d723becaa4c
sueszli <jabaryyahya@gmail.com>
2025-11-26 23:22:13 +0100

    up

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { fileURLToPath } from "url";
 import path from "path";
-import chalk from "chalk";
 import { getLlama, LlamaChatSession, resolveModelFile } from "node-llama-cpp";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -10,26 +9,9 @@ const modelPath = await resolveModelFile("hf:stabilityai/stable-code-instruct-3b
 const model = await llama.loadModel({ modelPath });
 const context = await model.createContext(); // to reduce memory footprint: `contextSize: {max: 8096}`
 const session = new LlamaChatSession({ contextSequence: context.getSequence() });
+const deterministicConfig = { temperature: 0, topK: 1, topP: 1.0, seed: 42 };
 
-const q1 = "Hi there, how are you?";
-console.log(chalk.yellow("User: ") + q1);
-const a1 = await session.prompt(q1);
-console.log(chalk.yellow("AI: ") + a1);
-console.log();
-
-const q2 = "What are the verbs in this sentence: 'The cat sat on the mat'";
-console.log(chalk.yellow("User: ") + q2);
-const responseGrammar = await llama.createGrammarForJsonSchema({
-    type: "object",
-    properties: {
-        verbs: {
-            type: "array",
-            items: {
-                type: "string",
-            },
-        },
-    },
-});
-const a2 = await session.prompt(q2, { grammar: responseGrammar });
-console.log(chalk.yellow("AI:"), responseGrammar.parse(a2).verbs);
-console.log();
+const q1 = "Write a small XPATH expression to select all the book titles in an XML document.";
+console.log("User: " + q1);
+const a1 = await session.prompt(q1, deterministicConfig);
+console.log("AI: " + a1);
